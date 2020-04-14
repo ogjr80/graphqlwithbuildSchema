dc7a1dea88eeb0d51bb03ec021661d0466dd29e7
Gideon <ogjr80@gmail.com>
2020-04-14 20:07:13 +0200

    added Mongodb and event

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const bodyParser = require('body-parser');
 const graphQLHTTP = require('express-graphql');
 const { buildSchema } = require('graphql');
 const _ = require('lodash');
+const mongoose = require('mongoose');
+const Event =  require('./models/event');
+
+
 
 
 const events = [];
@@ -43,24 +47,52 @@ app.use('/graphql', graphQLHTTP({
 `),
   rootValue: {
     events: () => {
-      return events;
+      return Event.find().then(events => {
+        return events.map(event => {
+          return {...event._doc};
+        })
+      }).catch(err => {
+        throw err;
+      })
     },
     createEvent: (args) => {
-      const event = {
-        _id : Math.random().toString(),
-        title: args.eventInput.title,
-        description: args.eventInput.description,
-        price: +args.eventInput.price,
-        date: new Date().toISOString()
-      }
-
-      events.push(event);
+      const event = new Event({
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          date: new Date(args.eventInput.date)
+      });
+     return event.save().then(result => {
+        console.log(result);
+        return {...result._doc}
+      }).catch(err => {
+        console.log(err);
+        throw err;
+      });
       return event;
     }
   },
   graphiql: true
-}))
+  })
 
-app.listen(3000, function(){
-  console.log('server running on port 3000');
-});
+);
+
+
+// mongoose.connect('mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@mgdbclust-lp87j.mongodb.net/eventdb?retryWrites=true&w=majority').then(()=> {
+//   app.listen(3000);
+// }).catch(err => {
+//   console.log(err);
+// });
+
+
+mongoose.connect('mongodb://localhost:27017/eventdb', { useNewUrlParser: true },function(){
+  console.log('database connection successfull');
+}).then(()=> {
+  app.listen(3000, function(){
+    console.log('server now running on port 3000');
+  })
+}).catch(() => console.log(err));
+//
+// app.listen(3000, function(){
+//   console.log('server running on port 3000');
+// })
